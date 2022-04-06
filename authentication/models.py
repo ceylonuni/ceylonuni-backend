2d315f4c9384b1908a20dccdf9be2639ebea6ce4
vniroshan <nnirosh447@gmail.com>
2022-04-06 07:36:42 +0530

    registerand validation apis added

--- authentication/models.py
+++ authentication/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from CeylonuniApp.models import Students
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your models here.
 
 
 class Accounts(models.Model):
     id = models.AutoField(primary_key=True)
-    email = models.CharField(max_length=150,unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=350)
     username = models.CharField(max_length=150,unique=True)
     verifiedDate = models.DateTimeField(auto_now_add=True)
@@ -15,12 +16,12 @@ class Accounts(models.Model):
     student = models.ForeignKey(
        Students, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return self.email
+   #  def __str__(self):
+   #      return self.email
 
-    # def tokens(self):
-    #     refresh = RefreshToken.for_user(self)
-    #     return {
-    #         'refresh': str(refresh),
-    #         'access': str(refresh.access_token)
-    #     }
+   #  def tokens(self):
+   #      refresh = RefreshToken.for_user(self)
+   #      return {
+   #          'refresh': str(refresh),
+   #          'access': str(refresh.access_token)
+   #      }
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics, status, views, permissions
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer, EmailSerializer, LoginSerializer, AccountVerificationSerializer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Accounts, Students
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+import jwt
+from django.conf import settings
+from drf_yasg.utils import swagger_auto_schema,APIView
+from drf_yasg import openapi
 # Create your views here.
 
 
@@ -15,4 +24,64 @@ class RegisterView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         user_data = serializer.data
+        user = Accounts.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('account-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(views.APIView):
+    serializer_class = EmailSerializer
+
+    @swagger_auto_schema( request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT, 
+    properties={
+        'email': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+    }))
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validate = True
+        if validate:
+             return Response({'email': 'This is an university mail'}, status=status.HTTP_200_OK)
+        
+
+class VerifyAccount(views.APIView):
+    serializer_class = AccountVerificationSerializer
+
+    token_param_config = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = Students.objects.get(id=payload['student'])
+            if not user.isVerified:
+                user.isVerified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
