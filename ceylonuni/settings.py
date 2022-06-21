04e87bb30f4292a6d5804e592409d3c6841db0c2
vniroshan <nnirosh447@gmail.com>
2022-06-22 00:07:31 +0530

    Allowed host url added

--- ceylonuni/settings.py
+++ ceylonuni/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ceylonuni.herokuapp.com']
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
