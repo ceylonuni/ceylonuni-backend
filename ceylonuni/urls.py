76ce272219874ce8697e7c7c8a58aa780ab645d1
vniroshan <nnirosh447@gmail.com>
2022-06-22 12:11:17 +0530

    urls updated

--- ceylonuni/urls.py
+++ ceylonuni/urls.py
@@ -22,8 +22,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from django.views.static import serve
-from django.conf.urls import url
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Ceylonuni API",
@@ -46,6 +44,4 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
